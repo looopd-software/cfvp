e3a2eacb072d4b0d360f12f6825ea8bccf549656
Alejandro Navarini <agi_navarin@contractor.indeed.com>
2024-07-09 00:07:45 -0300

    Add hero section desktop

--- src/app/components/sections/Navbar.tsx
+++ src/app/components/sections/Navbar.tsx
@@ -1,17 +1,19 @@
+// src/components/Navbar.tsx
 import Image from 'next/image';
 import React from 'react';
 import NavLinks from '../navbar/NavLinks';
-
-
+import Link from 'next/link';
 
 const Navbar: React.FC = () => (
-    <div className="container flex place-content-between relative z-10 items-center">
-        <Image
-            src={'/hero/isotipo.png'}
-            width={62 * 1.5}
-            height={76 * 1.5}
-            alt="Isotipo de Castro Fernandez Vignau y Pianovi"
-        />
+    <div className="absolute flex justify-between w-full container z-50">
+        <Link href={''}>
+            <Image
+                src={'/hero/isotipo.png'}
+                width={62 * 1.5}
+                height={76 * 1.5}
+                alt="Isotipo de Castro Fernandez Vignau y Pianovi"
+            />
+        </Link>
         <NavLinks />
     </div>
 );

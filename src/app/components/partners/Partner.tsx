4c892e2e7db1e0052c5c8db4a6b0344f8550feca
Alejandro Navarini <agi_navarin@contractor.indeed.com>
2024-07-10 22:03:34 -0300

    add partners section

---
+++ src/app/components/partners/Partner.tsx
@@ -0,0 +1,30 @@
+import Image from 'next/image';
+import { PartnerProps } from './PartnerProps';
+
+const Partner = ({ url, name, lastName, onClick, selected }: PartnerProps) => {
+  return (
+    <div
+      onClick={onClick}
+      className={`relative flex flex-col text-white gap-2 border-b-[3px] cursor-pointer ${selected ? 'border-primary' : ''}`}
+    >
+      <div className="relative w-48 h-60 rounded-sm overflow-hidden">
+        <Image
+          src={url}
+          alt={`Foto de ${name} ${lastName}`}
+          fill
+          style={{
+            objectFit: 'cover',
+            objectPosition: 'top',
+            mixBlendMode: selected ? 'normal' : 'luminosity',
+          }}
+        />
+      </div>
+      <div>
+        <p className="uppercase font-bold">{lastName} ,</p>
+        <p className="uppercase">{name}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Partner;

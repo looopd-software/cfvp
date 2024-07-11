90a4c7a36f80889232e5d8871e322dc46ec4c350
Agustin <agus.delmonti@gmail.com>
2024-07-11 20:07:38 -0300

    chore: Remove unnecessary Link component from ContactIconItem

--- src/app/components/form/ContactIconItem.tsx
+++ src/app/components/form/ContactIconItem.tsx
@@ -1,18 +1,10 @@
-import Link from 'next/link';
 import { ReactNode } from 'react';
 
-interface ContactIconItemProps {
-  href: string;
-  children: ReactNode;
-}
-
-const ContactIconItem = ({ href, children }: ContactIconItemProps) => {
+const ContactIconItem = ({ children }: { children: ReactNode }) => {
   return (
-    <Link href={href} passHref>
-      <div className="bg-[#1A1A1A] rounded-[4px] py-2 px-4 gap-2 inline-flex items-center text-white">
-        {children}
-      </div>
-    </Link>
+    <div className="bg-[#1A1A1A] rounded-[4px] py-2 px-4 gap-2 inline-flex items-center text-white">
+      {children}
+    </div>
   );
 };
 

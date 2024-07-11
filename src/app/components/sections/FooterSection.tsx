362fbeb51c7a60e09bcb5f20181211ae36f34a23
Alejandro Navarini <agi_navarin@contractor.indeed.com>
2024-07-11 00:58:36 -0300

    Add footer desktop section

---
+++ src/app/components/sections/FooterSection.tsx
@@ -0,0 +1,92 @@
+import Image from 'next/image';
+import ContactIconItem from '../form/ContactIconItem';
+import MapsIcon from '../form/MapsIcon';
+import EmailIcon from '../form/PhoneIcon';
+import WhatsAppIcon from '../form/WhatsAppIcon';
+import Link from 'next/link';
+
+const Logo = () => (
+  <Link href={''}>
+    <Image
+      src={'/hero/logo.svg'}
+      alt="Logo de Castro Fernandez Vignau y Pianovi"
+      width={223}
+      height={100}
+    />
+    <p className="text-primary uppercase max-w-[20ch] font-bold mt-2">
+      PROFESIONALES EN CIENCIAS ECONÓMICAS
+    </p>
+  </Link>
+);
+
+const ContactInfo = () => (
+  <div className="flex flex-col items-start gap-3 ">
+    <p className="font-bold mb-4">CONTACTO</p>
+    <Link
+      className="flex gap-2 hover:underline"
+      href="https://maps.app.goo.gl/fhrc2Egig33ris2q9"
+    >
+      <MapsIcon />
+      <span>Córdoba 1026, Rosario, Argentina</span>
+    </Link>
+    <Link className="flex gap-2 hover:underline" href="tel:+54 9 3416 25-3526">
+      <WhatsAppIcon />
+      <span>(704) 555-0127</span>
+    </Link>
+    <Link
+      className="flex gap-2 hover:underline"
+      href="mailto:contacto@cfvp.com.ar"
+    >
+      <EmailIcon />
+      <span>contacto@cfvp.com.ar</span>
+    </Link>
+  </div>
+);
+
+const NavigationLinks = () => (
+  <div className="flex flex-col items-start gap-3">
+    <p className="font-bold mb-4">NOSOTROS</p>
+    <Link className="hover:underline" href={'#servicios'}>
+      Servicios
+    </Link>
+    <Link className="hover:underline" href={'#nosotros'}>
+      Nosotros
+    </Link>
+    <Link className="hover:underline" href={'#socios'}>
+      Socios
+    </Link>
+    <Link className="hover:underline" href={'#clientes'}>
+      Clientes
+    </Link>
+    <Link className="hover:underline" href={'#contacto'}>
+      Contacto
+    </Link>
+  </div>
+);
+
+const FooterSection = () => {
+  return (
+    <section id="footer" className="relative bg-black pt-20 pb-4 text-white">
+      <div className="flex items-start w-full max-w-desktop container justify-between">
+        <Logo />
+        <div className="w-1/4"></div>
+        <ContactInfo />
+        <NavigationLinks />
+      </div>
+      <div className="flex items-start w-full max-w-desktop container justify-between mt-6">
+        <p>Castro, Fernández, Vignau & Pianovi © 2024 </p>
+        <p>
+          Made by{' '}
+          <Link
+            className="underline-offset-2 underline"
+            href={'https://looopd.com'}
+          >
+            Looopd
+          </Link>
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default FooterSection;

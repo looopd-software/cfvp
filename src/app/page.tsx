5c306e81d4941115d1fa27c768562393714d2b7a
Agustin <agus.delmonti@gmail.com>
2024-07-11 22:57:11 -0300

    chore: add lenis scrolling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React from 'react';
+import React, { useEffect } from 'react';
 import '@/app/globals.css';
 import { marcellus, karla } from './fonts';
 import HeroSection from './components/sections/HeroSection';
@@ -12,8 +12,22 @@ import PartnersSection from './components/sections/PartnersSection';
 import FormSection from './components/sections/FormSection';
 import FooterSection from './components/sections/FooterSection';
 import Navbar from './components/navbar/Navbar';
+import Lenis from 'lenis';
 
 export default function Page() {
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 2,
+    });
+
+    function raf(time: number) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+  }, []);
+
   return (
     <div
       className={`${marcellus.variable} ${karla.variable} font-sans overflow-x-hidden`}

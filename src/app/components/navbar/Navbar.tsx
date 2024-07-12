7887c6d673a1dd8ed811d658b2b8a267c1f12f49
Agustin <agus.delmonti@gmail.com>
2024-07-11 21:17:43 -0300

    chore: fixed position navbar

--- src/app/components/navbar/Navbar.tsx
+++ src/app/components/navbar/Navbar.tsx
@@ -1,11 +1,10 @@
-// src/components/Navbar.tsx
 import Image from 'next/image';
 import React from 'react';
 import NavLinks from './NavLinks';
 import Link from 'next/link';
 
 const Navbar: React.FC = () => (
-  <nav className="absolute w-full px-auto z-50">
+  <nav className="fixed top-0 w-full px-auto z-50">
     <div className="flex justify-between w-full container">
       <Link href={''}>
         <Image

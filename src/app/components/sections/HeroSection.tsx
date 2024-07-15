f16283679b75dd0360ca003609460c0df426be95
Alejandro Navarini <agi_anavarin@contractor.indeed.com>
2024-07-15 20:06:22 -0300

    Change opacity to 1 on exit

--- src/app/components/sections/HeroSection.tsx
+++ src/app/components/sections/HeroSection.tsx
@@ -37,7 +37,7 @@ export default function HeroSection() {
         <div className="relative w-full h-screen flex justify-center">
           <HeroVideo />
 
-          <div className="flex flex-col relative w-full z-10 items-center justify-center h-full gap-10 max-w-desktop">
+          <div className="flex flex-col relative w-full z-10 items-center justify-center h-full gap-10 max-w-desktop px-4">
             <motion.div
               className="relative w-full px-6 max-w-[757px] max-h-[332px]"
               initial="hidden"
@@ -58,7 +58,7 @@ export default function HeroSection() {
               />
             </motion.div>
             <motion.h1
-              className="uppercase text-center text-white text-[28px] leading-7 font-serif"
+              className="uppercase text-center text-white text-xl lg:text-[28px] leading-7 font-serif px-2"
               initial="hidden"
               animate="visible"
               variants={heroTextVariants}

5fdfba0a4fc2bf0346aaef103640061255aef50c
Alejandro Navarini <agi_anavarin@contractor.indeed.com>
2024-07-15 19:24:46 -0300

    Add show and hide animation

--- src/app/components/navbar/MobileNavbar.tsx
+++ src/app/components/navbar/MobileNavbar.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
+import { motion, AnimatePresence } from 'framer-motion';
 
 interface MobileMenuProps {
   isMenuOpen: boolean;
@@ -17,34 +18,49 @@ const links = [
   { href: '#contacto', text: 'Contacto' },
 ];
 
-const MobileMenu: React.FC<MobileMenuProps> = ({ isMenuOpen, toggleMenu }) => {
-  if (!isMenuOpen) return null;
+const menuVariants = {
+  hidden: { opacity: 0, x: '100%' },
+  visible: { opacity: 1, x: 0 },
+  exit: { opacity: 0, x: '100%' },
+};
 
+const MobileMenu: React.FC<MobileMenuProps> = ({ isMenuOpen, toggleMenu }) => {
   return (
-    <div className="absolute z-40 bg-black w-full h-screen top-0 flex flex-col items-center">
-      <Image
-        src={'/hero/logo.svg'}
-        alt="Logo de Castro Fernandez Vignau y Pianovi"
-        width={145}
-        height={69}
-        className="mt-3"
-      />
-      <div className="flex flex-col text-left w-full mt-6 text-white ">
-        {links.map((link, index) => (
-          <Link key={link.href} href={link.href} onClick={toggleMenu}>
-            <div
-              className={`py-4 pl-8 ${
-                index !== links.length - 1
-                  ? 'border-[#ffffff30] border-b-[0.5px]'
-                  : ''
-              }`}
-            >
-              {link.text}
-            </div>
-          </Link>
-        ))}
-      </div>
-    </div>
+    <AnimatePresence>
+      {isMenuOpen && (
+        <motion.div
+          className="absolute z-40 bg-black w-full h-screen top-0 flex flex-col items-center"
+          initial="hidden"
+          animate="visible"
+          exit="exit"
+          variants={menuVariants}
+          transition={{ type: 'spring', stiffness: 100, damping: 20 }}
+        >
+          <Image
+            src={'/hero/logo.svg'}
+            alt="Logo de Castro Fernandez Vignau y Pianovi"
+            width={145}
+            height={69}
+            className="mt-3"
+          />
+          <div className="flex flex-col text-left w-full mt-6 text-white ">
+            {links.map((link, index) => (
+              <Link key={link.href} href={link.href} onClick={toggleMenu}>
+                <div
+                  className={`py-4 pl-8 ${
+                    index !== links.length - 1
+                      ? 'border-[#ffffff30] border-b-[0.5px]'
+                      : ''
+                  }`}
+                >
+                  {link.text}
+                </div>
+              </Link>
+            ))}
+          </div>
+        </motion.div>
+      )}
+    </AnimatePresence>
   );
 };
 

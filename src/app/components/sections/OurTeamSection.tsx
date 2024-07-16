7dcfd3a13be0a3661ea5e16f563eb71429debff4
Alejandro Navarini <agi_anavarin@contractor.indeed.com>
2024-07-15 23:44:38 -0300

    Adapt team for mobile

--- src/app/components/sections/OurTeamSection.tsx
+++ src/app/components/sections/OurTeamSection.tsx
@@ -1,9 +1,88 @@
 import Image from 'next/image';
 import Heading from '../common/Heading';
+import { motion } from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
+
+const containerVariants = {
+  hidden: { opacity: 0 },
+  visible: {
+    opacity: 1,
+    transition: {
+      delayChildren: 0.3,
+      staggerChildren: 0.2,
+    },
+  },
+};
+
+const textVariants = {
+  hidden: { opacity: 0, y: 20 },
+  visible: { opacity: 1, y: 0 },
+};
+
+const imageVariants = {
+  hidden: { opacity: 0 },
+  visible: { opacity: 1 },
+};
 
 const OurTeamSection = () => {
+  const { ref, inView } = useInView({
+    triggerOnce: true,
+    threshold: 0.2,
+  });
+
   return (
-    <section id="equipo" className="relative py-32 z-0">
+    <section id="equipo" className="relative pt-7 lg:py-32 z-0" ref={ref}>
+      <TeamBackground />
+      <motion.div
+        className={`relative flex flex-col items-center justify-center max-w-desktop w-full lg:px-7 mx-auto gap-10 z-10 lg:flex-row lg:items-start`}
+        variants={containerVariants}
+        initial="hidden"
+        animate={inView ? 'visible' : 'hidden'}
+      >
+        <Heading text="NUESTRO EQUIPO" variant="light" />
+        <motion.div
+          className="w-full text-center lg:text-left lg:max-w-[700px]"
+          variants={containerVariants}
+        >
+          <motion.p
+            className="text-white lg:text-lg mb-12 px-7"
+            variants={textVariants}
+          >
+            Nuestro equipo está compuesto por especialistas calificados en las
+            diversas áreas de servicios que demanda el mercado, incluyendo
+            auditoría, impuestos, costos, laboral, economía, finanzas y
+            negocios.
+            <br />
+            <br />
+            Una trayectoria de más de 30 años nos permite combinar la
+            experiencia de los socios fundadores con la energía y el talento de
+            los socios más jóvenes.
+          </motion.p>
+          <motion.div
+            className="relative w-full h-[230px] rounded-lg lg:w-[700px]"
+            variants={imageVariants}
+          >
+            <Image
+              src="/our-team/our-team.jpg"
+              alt="Imagen de nuestro equipo trabajando"
+              fill
+              style={{
+                objectFit: 'cover',
+              }}
+              className="rounded-[4px]"
+            />
+          </motion.div>
+        </motion.div>
+      </motion.div>
+    </section>
+  );
+};
+
+export default OurTeamSection;
+
+function TeamBackground() {
+  return (
+    <>
       <div className="absolute top-0 left-0 w-full h-full z-0 opacity-50">
         <Image
           src="/our-team/our-team-bg.jpg"
@@ -16,33 +95,6 @@ const OurTeamSection = () => {
         />
       </div>
       <div className="absolute top-0 left-0 bg-black w-full h-full z-0 opacity-85"></div>
-
-      <div className="relative flex justify-center max-w-desktop w-full mx-auto gap-10 z-10">
-        <Heading text="NUESTRO EQUIPO" variant="light" />
-        <div className="max-w-[700px]">
-          <p className="text-white text-lg mb-12">
-            Nuestro equipo está compuesto por especialistas calificados en las
-            diversas áreas de servicios que demanda el mercado, incluyendo
-            auditoría, impuestos, costos, laboral, economía, finanzas y
-            negocios. Una trayectoria de más de 30 años nos permite combinar la
-            experiencia de los socios fundadores con la energía y el talento de
-            los socios más jóvenes.
-          </p>
-          <div className="relative w-[700px] h-[230px] rounded-lg">
-            <Image
-              src="/our-team/our-team.jpg"
-              alt="Imagen de nuestro equipo trabajando"
-              fill
-              style={{
-                objectFit: 'cover',
-              }}
-              className="rounded-[4px]"
-            />
-          </div>
-        </div>
-      </div>
-    </section>
+    </>
   );
-};
-
-export default OurTeamSection;
+}

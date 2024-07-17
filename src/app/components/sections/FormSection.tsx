5c0145cb5dd432799a0dd4952d65013dda98668d
Alejandro Navarini <agi_anavarin@contractor.indeed.com>
2024-07-16 22:32:26 -0300

    Add animations to form

--- src/app/components/sections/FormSection.tsx
+++ src/app/components/sections/FormSection.tsx
@@ -2,11 +2,36 @@ import Image from 'next/image';
 import Heading from '../common/Heading';
 import Form from '../form/Form';
 import ContactIcons from '../form/ContactIcons';
+import { motion } from 'framer-motion';
+import { useRef } from 'react';
+import { useInView } from 'react-intersection-observer';
 
 const FormSection = () => {
+  const { ref, inView } = useInView({
+    triggerOnce: true,
+    threshold: 0.2,
+  });
+
+  const containerVariants = {
+    hidden: { opacity: 0, x: -100 },
+    visible: {
+      opacity: 1,
+      x: 0,
+      transition: {
+        duration: 0.5,
+        staggerChildren: 0.3,
+      },
+    },
+  };
+
   return (
-    <section id="contacto" className="relative bg-white">
-      <div className="relative flex flex-col-reverse lg:flex-row w-full h-full gap-10 lg:gap-20 ">
+    <section id="contacto" className="relative bg-white" ref={ref}>
+      <motion.div
+        variants={containerVariants}
+        initial="hidden"
+        animate={inView ? 'visible' : 'hidden'}
+        className="relative flex flex-col-reverse lg:flex-row w-full h-full gap-10 lg:gap-20 "
+      >
         <div className="relative w-full h-[50vh] lg:h-auto lg:w-2/5">
           <Image
             src="/form/form-bg.jpg"
@@ -20,7 +45,7 @@ const FormSection = () => {
           <Heading variant="dark" text="CONTACTANOS" />
           <Form />
         </div>
-      </div>
+      </motion.div>
     </section>
   );
 };

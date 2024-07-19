150f444288c72a6caaadec099437f72ce04ae422
Agustin <agus.delmonti@gmail.com>
2024-07-18 21:16:41 -0300

    styles: mobile footer

--- src/app/components/sections/FooterSection.tsx
+++ src/app/components/sections/FooterSection.tsx
@@ -1,52 +1,50 @@
 import Image from 'next/image';
-import MapsIcon from '../form/MapsIcon';
-import EmailIcon from '../form/PhoneIcon';
-import WhatsAppIcon from '../form/WhatsAppIcon';
 import Link from 'next/link';
 
-const Logo = () => (
-  <Link href={''}>
-    <Image
-      src={'/hero/logo.svg'}
-      alt="Logo de Castro Fernandez Vignau y Pianovi"
-      width={223}
-      height={100}
-    />
-    <p className="text-primary uppercase max-w-[20ch] font-bold mt-2">
-      PROFESIONALES EN CIENCIAS ECONÓMICAS
-    </p>
-  </Link>
-);
-
 const ContactInfo = () => (
-  <div className="flex flex-col items-start gap-3 ">
-    <p className="font-bold mb-4">CONTACTO</p>
-    <Link
-      className="flex gap-2 hover:underline"
-      href="https://maps.app.goo.gl/fhrc2Egig33ris2q9"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      <MapsIcon />
-      <span>Córdoba 1026, Rosario, Argentina</span>
-    </Link>
-    <Link className="flex gap-2 hover:underline" href="tel:+54 9 3416 25-3526">
-      <WhatsAppIcon />
-      <span>(704) 555-0127</span>
-    </Link>
-    <Link
-      className="flex gap-2 hover:underline"
-      href="mailto:contacto@cfvp.com.ar"
-    >
-      <EmailIcon />
-      <span>contacto@cfvp.com.ar</span>
-    </Link>
+  <div className="flex-col justify-end items-start gap-4 inline-flex">
+    <p className="text-stone-200 text-sm font-bold leading-[21px]">CONTACTO</p>
+    <div className="flex-col justify-end items-start gap-2 inline-flex">
+      <Link
+        className="justify-center items-start gap-2 inline-flex hover:underline"
+        href="https://maps.app.goo.gl/fhrc2Egig33ris2q9"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <div className="w-4 h-[20.85px] relative">
+          <Image
+            src={'/icons/location.svg'}
+            alt="Icono de ubicación en mapa"
+            fill
+          />
+        </div>
+        <span>Córdoba 1026, Rosario, Argentina</span>
+      </Link>
+      <Link
+        className="justify-center items-start gap-2 inline-flex hover:underline"
+        href="tel:+54 9 3416 25-3526"
+      >
+        <div className="w-4 h-[20.85px] relative">
+          <Image src={'/icons/whatsapp.svg'} alt="Icono de WhatsApp" fill />
+        </div>
+        <span>(704) 555-0127</span>
+      </Link>
+      <Link
+        className="justify-center items-start gap-2 inline-flex hover:underline"
+        href="mailto:contacto@cfvp.com.ar"
+      >
+        <div className="w-4 h-[20.85px] relative">
+          <Image src={'/icons/email.svg'} alt="Icono de email" fill />
+        </div>
+        <span>contacto@cfvp.com.ar</span>
+      </Link>
+    </div>
   </div>
 );
 
 const NavigationLinks = () => (
   <div className="flex flex-col items-start gap-3">
-    <p className="font-bold">NOSOTROS</p>
+    <p className="text-stone-200 text-sm font-bold leading-[21px]">NOSOTROS</p>
     <div className="flex flex-col items-start gap-1">
       <Link
         className="font-normal text-sm hover:underline leading-[21px]"
@@ -84,26 +82,47 @@ const NavigationLinks = () => (
 
 const FooterSection = () => {
   return (
-    <section id="footer" className="relative bg-black pt-20 pb-4 text-white">
-      <div className="flex flex-col md:flex-row items-start w-full max-w-desktop container justify-between">
-        <Logo />
-        <div className="w-1/4"></div>
-        <ContactInfo />
-        <NavigationLinks />
-      </div>
-      <div className="flex flex-col md:flex-row items-start w-full max-w-desktop container justify-between mt-6">
-        <p>Castro, Fernández, Vignau & Pianovi © {new Date().getFullYear()}</p>
-        <p>
-          Desarrollado por{' '}
-          <Link
-            className="underline-offset-2 underline"
-            href="https://looopd.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Looopd
-          </Link>
-        </p>
+    <section id="footer" className="bg-black text-white">
+      <div className="container w-full px-7 py-10 flex-col justify-start items-start gap-[72px] inline-flex">
+        <div className="flex-col justify-start items-start gap-10 inline-flex">
+          <div className="w-full flex-col justify-center items-start gap-[7px] inline-flex">
+            <div className="justify-start items-start inline-flex">
+              <div className="w-[172px] h-[79px] pr-[7.38px] pb-[5.17px] justify-start items-center flex">
+                <Link href={''}>
+                  <Image
+                    src={'/hero/logo.svg'}
+                    alt="Logo de Castro Fernandez Vignau y Pianovi"
+                    width={223}
+                    height={100}
+                  />
+                </Link>
+              </div>
+            </div>
+            <div className="text-primary uppercase font-bold mt-2 leading-[17.92px]">
+              PROFESIONALES EN <br /> CIENCIAS ECONÓMICAS
+            </div>
+          </div>
+          <div className="w-full flex-col justify-start items-start gap-[34px] inline-flex">
+            <ContactInfo />
+            <NavigationLinks />
+          </div>
+        </div>
+        <div className="flex-col justify-start items-start gap-2 inline-flex font-light text-stone-200 text-sm">
+          <p>
+            Castro, Fernández, Vignau & Pianovi © {new Date().getFullYear()}
+          </p>
+          <p>
+            Desarrollado por{' '}
+            <Link
+              className="underline-offset-2 underline"
+              href="https://looopd.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Looopd
+            </Link>
+          </p>
+        </div>
       </div>
     </section>
   );

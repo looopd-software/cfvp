1a25c8f95cdf48adeaa56fdc158fa25c9212ebc2
Alejandro Navarini <agi_anavarin@contractor.indeed.com>
2024-07-26 10:32:05 -0300

    Add item to navbar. Rearrange

--- src/app/components/navbar/NavLinks.tsx
+++ src/app/components/navbar/NavLinks.tsx
@@ -4,8 +4,9 @@ interface Props {
 }
 const NavLinks: React.FC<Props> = ({ className }: Props) => (
   <div className={`text-white gap-12 items-center ${className}`}>
-    <NavLink href="#servicios">Servicios</NavLink>
     <NavLink href="#nosotros">Nosotros</NavLink>
+    <NavLink href="#servicios">Servicios</NavLink>
+    <NavLink href="#clientes">Clientes</NavLink>
     <NavLink href="#contacto">
       <div className="border border-white rounded-lg px-5 py-1">Contacto</div>
     </NavLink>
